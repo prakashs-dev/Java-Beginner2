19d4d798f979980720d6a201a287e2a0dcc57f2e
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2024-06-21 18:14:06 +0530

    Add files via upload

--- zoho/Assignment.java
+++ zoho/Assignment.java
@@ -2,8 +2,10 @@ package com.zoho;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 class Demonstration {
@@ -15,8 +17,7 @@ class Demonstration {
 				sb.append(str.trim() + ",");
 			}
 		}
-//		String ans = sb.toString();
-		return sb.substring(0, sb.length()-1).toString();
+		return sb.substring(0, sb.length() - 1).toString();
 	}
 
 	public static int Levenshtein(String str1, String str2) {
@@ -54,7 +55,6 @@ class Demonstration {
 		List<String> list1 = Arrays.asList(st1);
 		List<String> list2 = Arrays.asList(st2);
 
-
 		List<String> unions = new ArrayList<>();
 		unions.addAll(list1);
 		for (String s : list2) {
@@ -65,16 +65,16 @@ class Demonstration {
 		List<String> intersect = new ArrayList<>();
 		intersect.addAll(list1);
 		intersect.retainAll(list2);
-		
+
 		List<String> oddList1 = new ArrayList<>();
 		for (String odd : list1) {
-			if (Integer.parseInt(odd)%2==0) {
+			if (Integer.parseInt(odd) % 2 == 0) {
 				oddList1.add(odd);
 			}
 		}
 		List<String> evenList2 = new ArrayList<>();
 		for (String even : list2) {
-			if (Integer.parseInt(even)%2==1) {
+			if (Integer.parseInt(even) % 2 == 1) {
 				evenList2.add(even);
 			}
 		}
@@ -86,20 +86,54 @@ class Demonstration {
 
 	private static void result(List<String> unions) {
 		for (String num : unions) {
-			System.out.print(num+" ");
+			System.out.print(num + " ");
 		}
 		System.out.println();
 	}
+
+	public static int calculateChecksum(String n) {
+
+		int sum = 0;
+		for (int i = n.length() - 1; i >= 0; i--) {
+			int digit = Character.getNumericValue(n.charAt(i));
+			// Rightmost position starts at 0
+			int position = n.length() - 1 - i;
+			// Double the digits at even positions
+			if (position % 2 == 0) {
+				digit *= 2;
+				// Subtract 9 if the result is >= 10
+				if (digit >= 10) {
+					digit -= 9;
+				}
+			}
+			sum += digit;
+		}
+
+		// If the result is a multiple of 10, checksum digit will be 0
+		if (sum % 10 == 0) {
+			return 0;
+		} else {
+			// Otherwise, checksum digit is the num required to make the sum a multiple of
+			// 10
+			return 10 - (sum % 10);
+		}
+
+	}
+
 }
 
 public class Assignment {
 
-	public static void main(String[] args) {
-		String s = "			  ab  			 cd  				 ef					";
-		String res = Demonstration.solution(s);
-		System.out.println(res);
+	private static final String[] KEYPAD = { " .,", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+	private static Map<String, Boolean> memo = new HashMap<>();
 
-//		String str1 = "flaw", str2 = "lawn";
+	public static void main(String[] args) {
+
+//		String s = "			  ab  			 cd  				 ef					";
+//		String res = Demonstration.solution(s);
+//		System.out.println(res);
+
+//		String str1 = "kitten", str2 = "sitting";
 //		System.out.println(Demonstration.Levenshtein(str1, str2));
 
 		// Union and Intersection 4
@@ -108,5 +142,123 @@ public class Assignment {
 		String arr = "1 7 5 8";
 //		Demonstration.union(ar, arr);
 
+//		String num = "13";
+//		System.out.println(Demonstration.calculateChecksum(num));
+
+//		print diamond pattern
+//		int n = 3;
+//		printDiamondPattern(n);
+
+//		phone diractory
+//		String input = "44335550555666196667775553";
+//		System.out.println(translateKeystrokes(input));
+
+		List<Integer> nums = Arrays.asList(1,1);
+		System.out.println(isGoodArray(nums));
+
+//		String word = "ab", pattern = ".*";
+//		System.out.println(isMatchHelper(word, pattern));
+
+	}
+
+	private static boolean isMatchHelper(String word, String pattern) {
+
+		return isMatchHelper(word, pattern, 0, 0);
+	}
+
+	private static boolean isMatchHelper(String word, String pattern, int i, int j) {
+
+		String key = i + "," + j;
+		if (memo.containsKey(key)) {
+			return memo.get(key);
+		}
+		if (j == pattern.length()) {
+			return i == word.length();
+		}
+
+		boolean firstMatch = (i < word.length() && (word.charAt(i) == pattern.charAt(j) || pattern.charAt(j) == '.'));
+		boolean result;
+
+		if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+			result = (isMatchHelper(word, pattern, i, j + 2) || (firstMatch && isMatchHelper(word, pattern, i + 1, j)));
+		} else {
+			result = firstMatch && isMatchHelper(word, pattern, i + 1, j + 1);
+		}
+		memo.put(key, result);
+		return result;
+
+	}
+
+	private static boolean isGoodArray(List<Integer> nums) {
+
+		int length = nums.size();
+		if (length < 1)
+			return false;
+
+		Map<Integer, Integer> freqNum = new HashMap<>();
+		for (int i : nums) {
+			freqNum.put(i, freqNum.getOrDefault(i, 0) + 1);
+		}
+		for (int i = 1; i < length; i++) {
+			if (freqNum.getOrDefault(i, 0) != 1) {
+				return false;
+			}
+		}
+		return freqNum.getOrDefault(length, 0) == 2;
+
+	}
+
+	private static String translateKeystrokes(String input) {
+
+		StringBuilder result = new StringBuilder();
+		char[] chars = input.toCharArray();
+		int n = chars.length;
+
+		for (int i = 0; i < n; i++) {
+			if (chars[i] != '0') {
+				char currentChar = chars[i];
+				int count = 1;
+
+				// Count consecutive identical keystrokes
+				while (i + 1 < n && chars[i + 1] == currentChar) {
+					count++;
+					i++;
+				}
+
+				// Find the corresponding alphabet
+				int keyIndex = currentChar - '1';
+				if (keyIndex >= 0 && keyIndex < KEYPAD.length) {
+					String keyString = KEYPAD[keyIndex];
+					int charIndex = (count - 1) % keyString.length();
+					result.append(keyString.charAt(charIndex));
+				}
+			}
+		}
+		return result.toString();
+
+	}
+
+	private static void printDiamondPattern(int n) {
+//		n = 3 => 0, 1, 2
+		for (int i = 0; i < n; i++) {
+			printRow(i);
+		}
+		for (int i = n - 2; i >= 0; i--) {
+			printRow(i);
+		}
+	}
+
+	private static void printRow(int row) {
+
+		StringBuilder sb = new StringBuilder();
+//		4
+		for (int j = 0; j <= row * 2; j++) {
+			if (j % 2 == 0) {
+				sb.append('1');
+			} else {
+				sb.append('0');
+			}
+		}
+		System.out.println(sb.toString());
 	}
 }
